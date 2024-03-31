358f6fe41ea2a0fb89c71d2dd1c2ab052237ab02
Felipe Faria Semensato <felipef.semensato@gmail.com>
2024-03-31 02:05:29 -0300

    começo do body

---
+++ src/app/Components/Body/body.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import '../Body/style.css';
+
+
+const SalesforceCustomer360 = () => {
+  return (
+    <body>
+    <div className='write'>
+      <h2 className='title'>Entregue sucesso agora <br/> com o Salesforce <br/> Customer 360</h2>
+      <p>
+        O Customer 360 é a nossa suite de produtos e serviços que <br/> ajuda 98% dos clientes a alcançar ou superar suas metas de <br/> ROI.
+      </p>
+    </div> 
+    <div className='buttons'>
+      <button className='free-test-button'>
+        Faça o teste grátis
+      </button>
+      <button className='know-button'>
+        Saiba mais
+      </button>
+    </div>  
+    </body>
+  );
+};
+
+export default SalesforceCustomer360;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./Components/Header/header";
+import Body from "./Components/Body/body"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body>
         <Header />
         {children}
+        <Body />
         </body>
     </html>
   );
